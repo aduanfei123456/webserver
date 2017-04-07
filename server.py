404148d2cd7e3bb5176005cd424dfd82abe76417
aduanfei123456 <564103067@qq.com>
2017-04-07 16:27:18 +0800

    handlers

--- server.py
+++ server.py
@@ -4,8 +4,48 @@ import os
 class ServerException(Exception):
     '''for internal error reporting  '''
     pass
+class case_handler(object):
+    def __init__(self):
+        pass
+    def act(self,handler):
+        raise NotImplementedError()
+    def test(self,handler):
+        raise NotImplementedError()
+    def __call__(self, handler, **kwargs):
+        if self.test(handler):
+            self.act(handler)
+            return True
+        return False
+class case_no_file(case_handler):
+    def test(self,handler):
+        return not os.path.exists(handler.full_path)
+    def act(self,handler):
+        raise ServerException("{0} not found".format(handler.path))
+class case_existing_file(case_handler):
+    def test(self,handler):
+        return  os.path.isfile(handler.full_path)
+    def act(self,handler):
+        handler.handle_file(handler.full_path)
+class case_directory_index_file(case_handler):
+    ''' serve index.html page for a directory'''
+    def index_path(self,handler):
+        return os.path.join(handler.full_path,'index.html')
+    def test(self,handler):
+        return os.path.isdir(handler.full_path) and \
+        os.path.isfile(self.index_path(handler))
+    def act(self,handler):
+        handler.handle_file(self.index_path(handler))
+class case_always_fail(case_handler):
+    def test(self,handler):
+        return True
+    def act(self,handler):
+        raise ServerException("Unknow object{0}".format(handler.path))
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    ''' return file or error page '''
+
+    Cases=[case_no_file(),
+             case_existing_file(),
+             case_directory_index_file(),
+             case_always_fail()]
     Error_Page="""
      <html>
      <body>
@@ -19,13 +59,10 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
         try:
 
-            full_path=os.getcwd()+self.path.replace("/","\\")
-            if not os.path.exists(full_path):
-                raise ServerException("{path} not found".format(self.path))
-            elif os.path.isfile(full_path):
-                self.handle_file(full_path)
-            else:
-                raise ServerException("Unknow object {0}".format(self.path))
+            self.full_path=os.getcwd()+self.path.replace("/","\\")
+            for case in self.Cases:
+               if case(self):
+                  return
         except Exception as msg:
             self.handle_error(msg)
     def handle_file(self,full_path):
@@ -43,9 +80,9 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(content)
     def handle_error(self,msg):
-        page=self.Error_Page.format(self.path,msg)
+        page=self.Error_Page.format(path=self.path,msg=msg)
         self.send_content(page)
 if __name__=='__main__':
-        serverAddress=('',8000)
+        serverAddress=('',8081)
         server=BaseHTTPServer.HTTPServer(serverAddress,RequestHandler)
         server.serve_forever()
